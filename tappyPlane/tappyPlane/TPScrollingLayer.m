9ff5185d523ec74c7ce23663dfad10f70962a015
Anatoly Kovalev <inatoley@gmail.com>
2015-09-30 17:53:34 +0300

    yea

--- tappyPlane/tappyPlane/TPScrollingLayer.m
+++ tappyPlane/tappyPlane/TPScrollingLayer.m
@@ -35,6 +35,18 @@
 
 -(void)updateWithTimeElapsed:(NSTimeInterval)timeElapsed {
     [super updateWithTimeElapsed:timeElapsed];
+    if (self.scrolling && self.horyzontalScrollSpeed < 0 && self.scene) {
+        [self enumerateChildNodesWithName:@"Tile" usingBlock:^(SKNode * _Nonnull node, BOOL * _Nonnull stop) {
+            // Looking for position for node in our coordinates system
+            CGPoint nodePositionInScene = [self convertPoint:node.position toNode:self.scene];
+            if (nodePositionInScene.x + node.frame.size.width <
+                -self.scene.size.width * self.scene.anchorPoint.x) {
+                // If yes, we more the node
+                node.position = CGPointMake(self.rightMostTile.position.x + self.rightMostTile.size.width, node.position.y);
+                self.rightMostTile = (SKSpriteNode*)node;
+            }
+        }];
+    }
 }
 
 @end
