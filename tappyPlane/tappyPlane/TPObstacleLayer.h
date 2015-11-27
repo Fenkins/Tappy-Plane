2024daf2313c2701c6f378c64a2ef959af9c9ee5
Anatoly Kovalev <inatoley@gmail.com>
2015-11-27 22:24:07 +0300

    adding obstacles graphics and creating class

---
+++ tappyPlane/tappyPlane/TPObstacleLayer.h
@@ -0,0 +1,13 @@
+//
+//  TPObstacleLayer.h
+//  tappyPlane
+//
+//  Created by Fenkins on 27/11/15.
+//  Copyright © 2015 Fenkins. All rights reserved.
+//
+
+#import "TPScrollingNode.h"
+
+@interface TPObstacleLayer : TPScrollingNode
+
+@end
---
+++ tappyPlane/tappyPlane/TPObstacleLayer.m
@@ -0,0 +1,15 @@
+//
+//  TPObstacleLayer.m
+//  tappyPlane
+//
+//  Created by Fenkins on 27/11/15.
+//  Copyright © 2015 Fenkins. All rights reserved.
+//
+
+#import "TPObstacleLayer.h"
+
+@implementation TPObstacleLayer
+-(void)updateWithTimeElapsed:(NSTimeInterval)timeElapsed {
+    [super updateWithTimeElapsed:timeElapsed];
+}
+@end
